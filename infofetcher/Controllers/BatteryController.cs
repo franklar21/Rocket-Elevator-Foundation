04d8d24282340a4e41ae9cf1476c099b4c5f4efc
Mathieu <mathieu.houde@live.ca>
2018-10-30 16:49:15 -0400

    Get and Put battery status

--- infofetcher/Controllers/BatteryController.cs
+++ infofetcher/Controllers/BatteryController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using infofetcher.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace infofetcher.Controllers
 {
@@ -21,13 +22,52 @@ namespace infofetcher.Controllers
             return _context.Batteries.ToList ();
         }
 
-        [HttpGet ("{id}", Name = "GetBatteries")]
-        public ActionResult<Batteries> GetById (long id) {
-            var item = _context.Batteries.Find (id);
-            if (item == null) {
-                return NotFound ();
+        [HttpGet ("{id:int}", Name = "GetBatteryStatus")]
+        public string GetById (long id) {
+            var batteryStatus = _context.Batteries.Find(id).Status;
+            
+            if (batteryStatus == null) {
+                return "";
             }
-            return item;
+            return batteryStatus;
+        }
+        [HttpPut ("{id}", Name = "ChangeBatteryStatus")]
+        public string Update (long id, [FromBody] JObject body) {
+            
+            var battery = _context.Batteries.Find (id);
+            if (battery == null) {
+                return "Please enter a new status";
+            }
+
+            var newStatus = (string)body.SelectToken("status");
+            if (newStatus == "Active"  || (newStatus == "Inactive" ) || (newStatus == "Alarm" ) || (newStatus == "Intervention")){
+                var initialStatus = battery.Status;
+                battery.Status = newStatus;
+                _context.Batteries.Update (battery);
+                _context.SaveChanges ();
+                return "The status of battery #" + battery.Id + " successfuly changed from " + initialStatus + " to " + newStatus;
+            } else {
+                return "Invalid status: Must be Active, Inactive, Alarm or Intervention";
+            }
         }
     }
 }
+
+
+
+// [Route("{id:int}/details")]
+// [ResponseType(typeof(BookDetailDto))]
+// public async Task<IHttpActionResult> GetBookDetail(int id)
+// {
+//     var book = await (from b in db.Books.Include(b => b.Author)
+//                 where b.BookId == id
+//                 select new BookDetailDto
+//                 {
+//                     Title = b.Title,
+//                     Genre = b.Genre,
+//                     PublishDate = b.PublishDate,
+//                     Price = b.Price,
+//                     Description = b.Description,
+//                     Author = b.Author.Name
+//                 }).FirstOrDefaultAsync();
+// }
