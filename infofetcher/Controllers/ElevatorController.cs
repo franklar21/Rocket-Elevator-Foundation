0992807390468433a223c2ef0d7cd0a6c68cf563
jabsaidi <jabir.uredi@hotmail.com>
2018-10-30 21:02:54 -0400

    get not active elevators

--- infofetcher/Controllers/ElevatorController.cs
+++ infofetcher/Controllers/ElevatorController.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using infofetcher.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
 
 namespace infofetcher.Controllers {
     [Route ("api/elevators")]
@@ -26,6 +28,13 @@ namespace infofetcher.Controllers {
                 return NotFound ();
             }
             return item;
+        }
+
+        // Récupération d’une liste d’ascenseurs qui ne sont pas en opération au moment de la requête
+        [HttpGet ("status", Name = "GetNotActiveElevators")]
+        public ActionResult<List<Elevators>> Get (string status) {
+            var _result = _context.Elevators.Where(s=>s.status!="Operational");
+            return _result.ToList();
         }
         
     }
