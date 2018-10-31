dfeaf5b481f11cc62395358c97c8a8b6d1ec8cce
Mathieu <mathieu.houde@live.ca>
2018-10-31 19:11:54 -0400

    Fixes in BatteryController

--- infofetcher/Controllers/BatteryController.cs
+++ infofetcher/Controllers/BatteryController.cs
@@ -56,22 +56,3 @@ namespace infofetcher.Controllers
         }
     }
 }
-
-
-
-// [Route("{id:int}/details")]
-// [ResponseType(typeof(BookDetailDto))]
-// public async Task<IHttpActionResult> GetBookDetail(int id)
-// {
-//     var book = await (from b in db.Books.Include(b => b.Author)
-//                 where b.BookId == id
-//                 select new BookDetailDto
-//                 {
-//                     Title = b.Title,
-//                     Genre = b.Genre,
-//                     PublishDate = b.PublishDate,
-//                     Price = b.Price,
-//                     Description = b.Description,
-//                     Author = b.Author.Name
-//                 }).FirstOrDefaultAsync();
-// }
